4271e9051458d305516e240b42694b09293ae138
Egbert <Egbert@LenoAleks>
2023-07-16 16:35:19 +0200

    eaVideo is ready

--- public_html/app/app.js
+++ public_html/app/app.js
@@ -33,9 +33,9 @@ eaDir.directive('eaMaskHtml', [eaMaskHtml]).$inject = ['$scope'];
 eaDir.directive('eaMaskCss', [eaMaskCss]).$inject = ['$scope'];
 eaDir.directive('eaMaskJs', [eaMaskJs]).$inject = ['$scope'];
 eaDir.directive('eaMaskJson', [eaMaskJson]).$inject = ['$scope'];
+eaDir.directive('eaVideo', ['$rootScope', eaVideo]).$inject = ['$scope'];
 
 //eaDir.directive('eaNews', ['$rootScope', eaNews]).$inject = ['$scope'];
-//eaDir.directive('eaVideo', ['$rootScope', eaVideo]).$inject = ['$scope'];
 //eaDir.directive('eaCookies', ['$rootScope', '$cookies', '$compile', eaCookiesDirektive]).$inject = ['$scope'];
 
 // $compile, $rootScope
--- public_html/app/directive/eaVideo.js
+++ public_html/app/directive/eaVideo.js
@@ -16,11 +16,6 @@ var eaVideo = function ( $rootScope ) {
             $scope.vdoNo = $rootScope.paramsApp.vdoNo;
         },   // controller
         
-        // <ea-news data-news-title="News" 
-        //          data-news-mode="all" | "new" | "arc"
-        //          data-news-limit="0" | "1" | "2" ...
-        //          data-news-init-idx = "0" | "1...n">
-        //</ea-news>
         link: function (scope, ele, attrs) {      
             scope.vdoSrc = attrs.vdoSrc;
             scope.vdoBody = attrs.vdoBody;
