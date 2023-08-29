ec029edb93136ab418399c26a0b3f04472a3a06e
Egbert <Egbert@LenoAleks>
2023-08-29 18:32:28 +0200

    Directive eaProvideObj is ready for test- delete any object is missing

--- public_html/app/directive/eaProvideObj.js
+++ public_html/app/directive/eaProvideObj.js
@@ -9,9 +9,70 @@ var eaProvideObj = function ($rootScope) {
     // local scope 
     scope: true,
     
+    controller: function($scope) {
+        $scope.objZero = {};
+        $scope.objNewArr = [];
+        
+        $scope.initObj = function(obj, arrName) {       
+            const keys = Object.keys(obj[arrName][0]);
+            let objRowZero = {};
+            
+            for(let i=0; i<keys.length; i++) {
+                let o0 = {};
+                o0[keys[i]] = "";
+                Object.assign(objRowZero, o0);
+            }
+            $scope.objZero = objRowZero;
+            
+            for(let o of obj[arrName]) {
+                let values = Object.values(o);
+                let objRow = {};
+                for(let i=0; i<keys.length; i++) {
+                    let o = {};
+                    o[keys[i]] = values[i];
+                    Object.assign(objRow, o);
+                }                
+                $scope.objNewArr.push(objRow);
+            }
+            return false;
+        };
+        
+        $scope.addObjRow = function() {
+            $scope.objNewArr.push($scope.objZero);
+        };
+        
+        $scope.deleteObjRow = function(idx) {
+            delete $scope.objNewArr[idx];
+            ($scope.objNewArr.length = 0)? $scope.objNewArr.push($scope.objZero) : "";
+            return false;
+        };
+        
+        $scope.saveObj = function() {
+            $scope.provideObj[$scope.provideObjArrName] = $scope.objNewArr;
+            return false;
+        };
+        
+    },
+    
     link: function (scope, ele, attrs) {
         scope.provideObj = $rootScope[attrs.provideObj]; 
+        scope.provideObjArrName = attrs.arrName; 
+        scope.initObj(scope.provideObj, scope.provideObjArrName);
+        
+        // Test EA
+        // objNewArr is a clone of $rootScope[newDeTicket] a reference by value!
+        scope.objNewArr[0].type="W";
+        console.log($rootScope[attrs.provideObj]);
+        scope.addObjRow();
+        //scope.deleteObjRow(1);
+        scope.saveObj();
+        console.log("new");
+        console.log(scope.objNewArr);
+        console.log("alt");
+        console.log($rootScope[attrs.provideObj]);
     }       
+    
+    
   };
 };
 
