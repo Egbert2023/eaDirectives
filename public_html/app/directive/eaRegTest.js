3481eac44ca32bbe30253420f620c7204fbcc54d
Egbert <Egbert@LenoAleks>
2023-12-19 18:19:41 +0100

    RegEx in work 2

--- public_html/app/directive/eaRegTest.js
+++ public_html/app/directive/eaRegTest.js
@@ -4,39 +4,13 @@ var eaRegTest = function () {
   return {
     restrict: 'E',
     scope: true,
-    template: '<div class="row">' +
-              '<style>' +
-              '.eaMark {opacity: 0.5; font-weight:  bold; background-color: lightblue; display: inline-flex; position: relativ} ' +
-              '.eaNoMark {opacity: 0.5; font-weight:  bold; display: inline-flex; position: absolute;} ' +
-              '.eaGap {opacity: 0.5; font-weight: bold; display: inline-flex; position: absolute;} ' +
-              '</style>' +
-              ' <hr style="width: 95%" />' +
-              '  <div class="col-lg-4 col-md-6 col-sm-12">' +
-              '      <label for="inputRegPattern" class="bold">regEx:</label>' +
-              '      /<input type="text" ' +
-              '              id = "inputRegPattern"' +
-              '              ng-change="setResult()"' +
-              '              ng-model="regPattern"/>/{{regModyfier}}' +
-              '  </div>' +
-              '  <div class="col-lg-4 col-md-6 col-sm-12">' +
-              '      <label for="inputRegText" class="bold">Text:</label>' +
-              '      <input type="text" ' +
-              '              id = "inputRegText"' +
-              '              ng-change="setResult()"' +
-              '              ng-model="regTxt"/>' +
-              '  </div>' +
-              '  <div class="col-lg-4 col-md-12 col-sm-12"  >' +
-              '     <div ng-repeat="res in regResult">' +
-              '       <span class="eaNoMark">{{regTxt}}</span>' +
-              '       <span class="eaGap" >{{regGap[$index]}}</span><span class="eaMark">{{res}}</span>' +
-              '     </div>' +
-              '  </div>' +
-              ' <hr style="width: 95%"/>' +
-              '</div>',
+    templateUrl: "app/template/regTest.html",
     
     controller: function($scope) {
-        $scope.regModyfierArray = ["g", "i", "m"];
+        $scope.regModyfierArray = ["g", "i", "m", "gi"];
+        $scope.regFunctionArray = ["test()", "exec()", "match()", "search()"];
         $scope.regModyfier = "g";
+        $scope.regFunction = "test()";
         $scope.regResult = [];
         $scope.regGap = [];
         $scope.regTxt = "";
