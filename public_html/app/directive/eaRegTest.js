aa66fdf2bf892b29f430a028bea2d7f1d254bb7d
Egbert <Egbert@LenoAleks>
2024-01-05 13:52:44 +0100

    RegEx in work 4 - spaces at start ore end of string make problems

--- public_html/app/directive/eaRegTest.js
+++ public_html/app/directive/eaRegTest.js
@@ -17,19 +17,25 @@ var eaRegTest = function () {
         $scope.regPattern = "";
         
         $scope.setResult = function() {
-//            $scope.regTxt = $scope.regTxt.replace(" ", "&nbsp;");
+            //$scope.regTxt = $scope.regTxt.replaceAll(" ", "&nbsp;");
             $scope.regResult = [];
             $scope.regGap = [];
+            $scope.regRest = [];
             let res = "";
             let regEx = RegExp($scope.regPattern, $scope.regModyfier);
             let regResult = regEx.exec($scope.regTxt);  // this is an array
             $scope.regResult = regResult;
             for(let idx = 0; idx < regResult.length; idx++) {
                 let regGap = "";
+                let regRest = "";
                 let res = (regResult[idx])? regResult[idx] : "";
                 let idxOff = $scope.regTxt.indexOf(res);
                 regGap = (idxOff>0)? $scope.regTxt.substring(0, idxOff) : "";
+                let idxRest = regGap.length + res.length;
+                regRest = (idxRest < $scope.regTxt.length)? $scope.regTxt.substring(0, idxRest) : "";
+                
                 $scope.regGap.push(regGap);
+                $scope.regRest.push(regRest);
             };
             //$scope.regResult = ($scope.regResult)? $scope.regResult.toString() : "";
             //let idxOff = $scope.regTxt.indexOf($scope.regResult);
@@ -39,11 +45,12 @@ var eaRegTest = function () {
         $scope.encodeSpace = function(inStr) {
             let ret = inStr.replaceAll(" ", "&nbsp;");
 
-            // Am ANfang und am Ende eines Strings werden die Spacec weggetrimmt.
+            // Am Anfang und am Ende eines Strings werden die Spacec weggetrimmt.
             // Entweder muss dies gezählt werden und ein addGap Array eingefügt werden oder
-            // Eine ander Darstellungsform (nicht übereinander4 schreiben) gewählt werden.
+            // Eine ander Darstellungsform (nicht übereinander schreiben) gewählt werden.
 
-            ret = inStr;
+            ret =  inStr;
+            
             return ret;
         };
         
